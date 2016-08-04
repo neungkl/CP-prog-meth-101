c55746f6e2abfb11fefd0f103182c0b40d15027a
neungkl <kosatelim@gmail.com>
2016-08-04 11:26:56 +0700

    Update

---
+++ Graphics-Template/src/Circle.java
@@ -0,0 +1,24 @@
+
+public class Circle {
+	private int x;
+	
+	public Circle() { x = 0; }
+	public void update() { 
+//		x += 5; 
+//		if(x > 600) x = 0;
+	}
+	
+	public void moveLeft() {
+		x -= 5;
+		if(x < 0) x = 600 - 100;
+	}
+	
+	public void moveRight() {
+		x += 5;
+		if(x > 600) x = 0;
+	}
+	
+	public int getX() {
+		return x;
+	}
+}
--- Graphics-Template/src/GameScreen.java
+++ Graphics-Template/src/GameScreen.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints.Key;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
@@ -12,6 +13,7 @@ import javax.swing.JComponent;
 public class GameScreen extends JComponent {
 	
 	private BufferedImage OImg;
+	private Circle circle;
 	
 	public GameScreen() {
 		try {
@@ -31,9 +33,15 @@ public class GameScreen extends JComponent {
 			
 			@Override
 			public void keyPressed(KeyEvent e) {
-				System.out.println(e.getKeyCode());
+				if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+					circle.moveLeft();
+				} else if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+					circle.moveRight();
+				}
 			}
 		});
+		
+		circle = new Circle();
 	}
 	
 	@Override
@@ -42,9 +50,14 @@ public class GameScreen extends JComponent {
 		
 		Graphics2D g2d = (Graphics2D) g;
 		
-		g2d.setColor(Color.WHITE);
-		g2d.fillRect(0, 0, 600, 600);
+		g2d.setColor(Color.RED);
 		
-		g2d.drawImage(OImg, 250, 250, 100, 100, null);
+		circle.update();
+		g2d.fillOval(circle.getX(), 600 - 140, 100, 100);
+		
+		//g2d.setColor(Color.WHITE);
+		//g2d.fillRect(0, 0, 600, 600);
+		
+		//g2d.drawImage(OImg, 250, 250, 100, 100, null);
 	}
 }
